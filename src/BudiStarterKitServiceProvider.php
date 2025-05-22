2c6fdb2609615d66620a3bc772540a7b504533d7
pascalprfst <dev@pascal-prfst.net>
2025-05-22 18:56:00 +0200

    added: AdminController & route and updated Install Command

--- src/BudiStarterKitServiceProvider.php
+++ src/BudiStarterKitServiceProvider.php
@@ -2,17 +2,29 @@
 
 namespace Budi\BudiStarterkit;
 
-use Illuminate\Support\ServiceProvider;
 use Budi\BudiStarterkit\Commands\InstallCommand;
+use Illuminate\Support\ServiceProvider;
 
-class BudiStarterkitServiceProvider extends ServiceProvider
+class BudiStarterKitServiceProvider extends ServiceProvider
 {
-    public function boot() : void
+    public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../stubs/resources/views' => resource_path('views'),
             ], 'budi-starterkit-views');
+
+            $this->publishes([
+                __DIR__ . '/../stubs/resources/js' => resource_path('js'),
+            ], 'budi-starterkit-scripts');
+
+            $this->publishes([
+                __DIR__ . '/../stubs/app/Http/Controllers' => app_path('Http/Controllers'),
+            ], 'budi-starterkit-code');
+
+            $this->publishes([
+                __DIR__ . '/../stubs/routes/web.php' => base_path('routes/web.php'),
+            ], 'budi-starterkit-routes');
 
             $this->commands([
                 InstallCommand::class,
