dbae077d82c1fffe92e010b21aff176ea4f8e38d
pascalprfst <dev@pascal-prfst.net>
2025-05-27 18:50:29 +0200

    updated: app layout

---
+++ stubs/resources/views/components/sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="w-72 bg-gray-900 h-screen shadow-md">
+    <div class="w-full py-4 px-4 border-b border-gray-700 flex gap-x-4">
+        <div class="w-14 h-14 bg-white rounded-tr-lg rounded-bl-lg">
+
+        </div>
+        <div>
+            <div class="text-lg font-medium uppercase text-gray-200">Companyname</div>
+            <div class="text-gray-400">Slogan here</div>
+        </div>
+    </div>
+</div>
--- stubs/resources/views/layouts/app.blade.php
+++ stubs/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="overflow-hidden" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,12 +9,22 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
 </head>
 <body class="font-sans antialiased">
-<div class="min-h-screen bg-slate-50">
-    <main>
-        {{ $slot }}
-    </main>
+<x-toast/>
+<div class="min-h-screen bg-slate-50 flex">
+    <x-sidebar/>
+    <div class="w-full">
+        <x-header/>
+        <main class="py-6 px-8 overflow-y-auto  h-[calc(100vh-48px)]">
+            {{ $slot }}
+        </main>
+    </div>
 </div>
 @livewireScripts
 </body>
