4eaeef59cb7747e419ebe55cf2fdda80d9104496
pascalprfst <dev@pascal-prfst.net>
2025-05-28 18:50:17 +0200

    button component medium corners base

--- stubs/resources/views/components/button.blade.php
+++ stubs/resources/views/components/button.blade.php
@@ -9,13 +9,13 @@
         default => 'px-4 py-3 text-sm',
     };
 
-    $baseClasses = 'inline-flex items-center rounded-sm font-medium tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150 ' . $sizeClasses;
+    $baseClasses = 'inline-flex items-center rounded-md font-medium tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150 ' . $sizeClasses;
     $baseColors = 'text-white bg-black hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-900 disabled:bg-gray-300 disabled:text-gray-100 focus:ring-gray-700';
 
     $classes = $baseClasses . ' ' .
                 ($danger ? 'text-white bg-red-500 hover:bg-red-600 focus:bg-red-600 active:bg-red-700 disabled:bg-red-200 disabled:text-red-50 focus:ring-red-500' :
                 ($secondary ? 'text-gray-800 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 disabled:bg-gray-100 disabled:text-gray-300 focus:ring-gray-300' :
-                ($outline ? 'text-gray-700 relative bg-white hover:bg-black hover:text-white focus:bg-gray-700 focus:text-white active:bg-gray-800 disabled:text-gray-300 disabled:bg-transparent before:disabled:border-gray-300 focus:ring-gray-700 before:absolute before:inset-0 before:border before:border-gray-700 before:rounded-sm' : $baseColors))) . ' ' .
+                ($outline ? 'text-gray-700 relative bg-white hover:bg-black hover:text-white focus:bg-gray-700 focus:text-white active:bg-gray-800 disabled:text-gray-300 disabled:bg-transparent before:disabled:border-gray-300 focus:ring-gray-700 before:absolute before:inset-0 before:border before:border-gray-700 before:rounded-mdc' : $baseColors))) . ' ' .
                 ($uppercase ? 'uppercase' : '');
 
 @endphp
